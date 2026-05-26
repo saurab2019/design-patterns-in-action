1a10318c3d5585069863cc3633079cea0760cff7
Saurabh Garg <56071783+saurab2019@users.noreply.github.com>
2026-05-26 22:54:35 +0530

    Added ItertorPattern Test Drive

---
+++ DesignPatterns/IteratorPattern/IteratorPatternTestDrive.cs
@@ -0,0 +1,11 @@
+namespace DesignPatterns.IteratorPattern
+{
+    public class IteratorPatternTestDrive : TestDrive
+    {
+        public static void test()
+        {
+            /* Write the code here to test the implementation of the Iterator Pattern */
+             Console.WriteLine("Iterator Pattern Test Drive");
+        }
+    }
+}
